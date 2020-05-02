e3b0cb766da90bf240aef9e428742132abba5d8e
Michael Nnaji <mnnaji3@uwo.ca>
2020-05-02 02:10:52 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,6 +1,12 @@
 require_relative "./vehicle.rb"
 
 class Car < Vechicle
+attr_accessor :wheel_size, :wheel_number
+
+def initialize(wheel_size,wheel_number)
+  @wheel_size = wheel_size
+  @wheel_number = wheel_number
+end
 
 
 
