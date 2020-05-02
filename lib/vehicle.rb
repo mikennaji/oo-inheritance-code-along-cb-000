24c0cfa552b69de37c0431b7102efd0c9c513c39
Michael Nnaji <mnnaji3@uwo.ca>
2020-05-02 02:09:08 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,4 +1,18 @@
 class Vechicle
+attr_accessor :wheel_size, :wheel_number
+
+def initialize(wheel_size,wheel_number)
+  @wheel_size = wheel_size
+  @wheel_number = wheel_number
+end
+
+def go 
+  return "vrrrrrrrooom!"
+end
+
+def fill_up_tank
+  return "filling up!"
+end
 
 
 
